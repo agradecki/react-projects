88b734ac4c4407d2ff8cde87e694c5549dd532df
Alan <alangradecki@wp.pl>
2020-12-17 19:34:34 +0100

    created a controled form and added css

--- form_with_validation/src/components/App.js
+++ form_with_validation/src/components/App.js
@@ -1,7 +1,88 @@
+import { Component } from "react";
+
 import "./App.css";
 
-function App() {
-  return <div>REACT</div>;
+class App extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    accept: false,
+  };
+
+  handleChange = (e) => {
+    const name = e.target.name; //pobiera name z odpowiedniego elementu/inputa formularza
+    const type = e.target.type;
+
+    if (type === "text" || type === "password" || type === "email") {
+      const value = e.target.value;
+
+      this.setState({
+        [name]: value, // [name] - to nazwa elementu z formularza oraz jednocześnie nazwa ze stanu
+      });
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+
+      this.setState({
+        [name]: checked,
+      });
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <form onSubmit={this.handleSubmit} noValidate>
+          {/**noValidate - wyłącza domyślna validacje html5 */}
+          <label htmlFor="user">
+            Twoje imię:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Twoje email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            Twoje hasło:
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="accept">
+            <input
+              type="checkbox"
+              id="accept"
+              name="accept"
+              checked={this.state.accept}
+              onChange={this.handleChange}
+            />
+            Wyrażam zgodę
+          </label>
+          <button>Zapisz się</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
