39201f2c9c661d47c8eb3d41dbbf21f0517225f7
Alan <alangradecki@wp.pl>
2020-12-18 21:21:36 +0100

    Added done section in Task.js

--- to-do-app/src/components/Task.js
+++ to-do-app/src/components/Task.js
@@ -1,16 +1,36 @@
 import React from "react";
 
 const Task = (props) => {
-  const { text, date, id } = props.task;
-  return (
-    <div>
-      <p>
-        <strong>{text}</strong> - do <span>{date} </span>
-        <button onClick={() => props.change(id)}>Zostało zrobione</button>
-        <button onClick={() => props.delete(id)}>X</button>
-      </p>
-    </div>
-  );
-};
+  const style = {
+    color: "red",
+    fontWeight: "bold",
+  };
+  const { text, date, id, active, important, finishDate } = props.task;
 
+  if (active) {
+    return (
+      <div>
+        <p>
+          <strong style={important ? style : null}>{text}</strong> - do
+          <span>{date} </span>
+          <button onClick={() => props.change(id)}>Zostało zrobione</button>
+          <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </div>
+    );
+  } else {
+    const finish = new Date(finishDate).toLocaleString();
+
+    return (
+      <div>
+        <p>
+          <strong>{text}</strong>
+          <em>(zrobić do {date})</em>
+          <br />* potwierdzenie wykonania <span>{finish}</span>
+          <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </div>
+    );
+  }
+};
 export default Task;
