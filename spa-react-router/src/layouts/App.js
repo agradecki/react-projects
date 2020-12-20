240070330bfd5a9633fe66489938588efa4502cc
Alan <alangradecki@wp.pl>
2020-12-20 19:51:23 +0100

    Added navigation layouts and css

--- spa-react-router/src/layouts/App.js
+++ spa-react-router/src/layouts/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div className="app">
           <header>{<Header />}</header>
           <main>
             <aside>{<Navigation />}</aside>
--- spa-react-router/src/layouts/Navigation.js
+++ spa-react-router/src/layouts/Navigation.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
+
+const list = [
+  { name: "start", path: "/", exact: true },
+  { name: "produkty", path: "/products" },
+  { name: "kontakt", path: "/contact" },
+  { name: "panel admina", path: "/admin" },
+];
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const menu = list.map((item) => (
+    <li key={item.name}>
+      <NavLink to={item.path} exact={item.exact ? item.exact : false}>
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+
+  return (
+    <nav className="main">
+      <ul>{menu}</ul>
+    </nav>
+  );
 };
 
 export default Navigation;
