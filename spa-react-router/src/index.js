9f920dac5329c1090294f1117a1102c0de62f3cc
Alan <alangradecki@wp.pl>
2020-12-20 18:56:53 +0100

    Added files with css styles and layouts

--- spa-react-router/src/index.js
+++ spa-react-router/src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import "./styles/index.css";
+import App from "./layouts/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ spa-react-router/src/layouts/App.js
@@ -0,0 +1,29 @@
+import "../styles/App.css";
+import { Component } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Navigation from "./Navigation";
+import Page from "./Page";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className="page">
+              <Page />
+            </section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
