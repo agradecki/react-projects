725d34330ec3a57c0bd1c512df291280c8e54de1
Alan <alangradecki@wp.pl>
2020-12-21 13:50:42 +0100

    Added footer layout and css.

--- spa-react-router/src/layouts/Footer.js
+++ spa-react-router/src/layouts/Footer.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h2>Stopka</h2>
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <p>
+            Jesteś na <span>stronie głównej.</span>
+          </p>
+        )}
+      />
+
+      <Route
+        path="/:page"
+        render={(props) => (
+          <p>
+            Jesteś na <span>{props.match.params.page}</span> Jesteś na{" "}
+            <span>{props.match.url}</span> Jesteś na{" "}
+            <span>{props.match.path}</span>
+          </p>
+        )}
+      />
+
+      <Route
+        path="/:page/:idProduct"
+        render={(props) => (
+          <p>
+            Jesteś na <span>{props.match.params.idProduct}</span> Jesteś na{" "}
+            <span>{props.match.url}</span> Jesteś na{" "}
+            <span>{props.match.path}</span>
+          </p>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Footer;
