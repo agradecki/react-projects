c954cb5614358d0883a065b811b3d1c6c390bea6
Alan <alangradecki@wp.pl>
2020-12-21 11:25:59 +0100

    Added contact form and css styles.

--- spa-react-router/src/pages/ContactPage.js
+++ spa-react-router/src/pages/ContactPage.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
 
-const ContactPage = () => {
-  return <div>Kontakt</div>;
-};
+class ContactPage extends Component {
+  state = {
+    value: "",
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      value: "",
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wpisz wiadomość..."
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt
+          when={this.state.value}
+          message="Masz niewypełniony formularz. Czy na pewno chcesz opuścić tę stronę?"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
